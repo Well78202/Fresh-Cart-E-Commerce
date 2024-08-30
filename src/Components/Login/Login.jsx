0459e8978a9bf10b0d2cbe04ce04aec6ac790ba7
Well78202 <ahmed.walid78202@gmail.com>
2024-08-31 01:44:44 +0300

    done

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -17,7 +17,6 @@ export default function Login() {
       .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
       .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
       .matches(/[0-9]/, 'Password must contain at least one number')
-      .matches(/[!@#$%^&*(),.?":{}|<>]/, 'Password must contain at least one special character')
       .required('Password is required')
   });
 
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -19,7 +19,6 @@ export default function Register() {
       .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
       .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
       .matches(/[0-9]/, 'Password must contain at least one number')
-      .matches(/[!@#$%^&*(),.?":{}|<>]/, 'Password must contain at least one special character')
       .required('Password is required'),
     rePassword: Yup.string()
       .oneOf([Yup.ref('password')], 'Passwords must match')
