c0e72ca08700a0cc563f313c9d14d3daea1db61b
Well78202 <ahmed.walid78202@gmail.com>
2024-08-31 01:44:31 +0300

    done

--- src/Pages/CheckOut/CheckOut.jsx
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -23,7 +23,7 @@ export default function CheckOut() {
     try {
       setLoading(true);
       let { data } = await axios.post(
-        `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=https://myfresh-card.netlify.app/`,
+        `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=https://myfresh-card.netlify.app`,
         {
           shippingAddress,
         },
